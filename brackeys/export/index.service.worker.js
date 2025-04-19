62648f9c75e2d05eb8bb4cd12b4ec206e2f2e1d5
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2025-04-19 23:03:51 +0200

    Implement mouse button for jumping

--- brackeys/export/index.service.worker.js
+++ brackeys/export/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1745056893|94673179';
+const CACHE_VERSION = '1745096617|833416687';
 /** @type {string} */
 const CACHE_PREFIX = 'Brackeys-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
